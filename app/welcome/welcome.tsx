765fc6d2b9e25f0ea834cd68f493f1aaf33dda4f
Luiz Felipe <126274791+Lulu015@users.noreply.github.com>
2026-01-28 01:03:04 +0000

    Atualização da aula

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,11 +1,25 @@
 export function Welcome() {
-const titulo = "Hello Ocean";
+  const titulo = "Hello Zelda!!";
 
   return (
-    <div>
-    <h1>
-      {titulo}
-    </h1>
-    </div>
+    <>
+      <div className="header">
+        <h1>Rick and Morty</h1>
+      </div>
+      <div className="personagens">
+        <div className="personagem">
+          <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+          <p>
+            Rick Sanchez
+          </p>
+          <p>
+            Human
+          </p>
+          <span>
+            Alive
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
